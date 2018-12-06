0193a2437aed9b09ea04b0ef764aa514fe768dab
Rafael Scatizzi <rafaelzero01@gmail.com>
2018-12-06 09:22:46 -0500

    Primer Commit Vectores Como Parametro

--- VectoresPararm/Program.cs
+++ VectoresPararm/Program.cs
@@ -8,8 +8,28 @@ namespace VectoresPararm
 {
     class Program
     {
+        const int MAX = 10;
         static void Main(string[] args)
         {
+            int[] x = new int[MAX];
+            Leer(x);
+            Imprimir(x);
+            Console.ReadKey();
+        }
+        public static void Leer(int [] x)
+        {
+            for (int i=0;i<MAX;i++)
+            {
+                Console.WriteLine("\nIngrese el elemento {0}", i + 1);
+                x[i] = Int32.Parse(Console.ReadLine());
+            }
+        }
+        public static void Imprimir(int [] x)
+        {
+            foreach(int num in x)
+            {
+                Console.WriteLine("\nElemento {0}",num);
+            }
         }
     }
 }
